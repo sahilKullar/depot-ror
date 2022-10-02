937fde0dc04c515ab30c93a0fda1910b9bc19b21
Sahil Kullar <sahil_kullar@outlook.com>
2022-10-02 21:50:13 +0530

    product validations and unit testing

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand(1000)}"
   end
 
   test "should get index" do
@@ -19,7 +20,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_difference("Product.count") do
       post products_url,
            params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price,
-                                title: @product.title } }
+                                title: @title } }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -38,7 +39,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should update product" do
     patch product_url(@product),
           params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price,
-                               title: @product.title } }
+                               title: @title } }
     assert_redirected_to product_url(@product)
   end
 
