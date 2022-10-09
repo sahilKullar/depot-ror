51d7f761b7c3fe49d8578cadde12cbbb213b8467
Sahil Kullar <sahil_kullar@outlook.com>
2022-10-09 13:35:36 +0530

    Cart creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,16 @@ class Product < ApplicationRecord
   validates :image_url, allow_blank: true, format: { with: /\.(gif|jpg|png)\z/i,
                                                      message: "must be a URL for GIF, JPG or PNG image." }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  # ensure that there are no line items referencing this product 
+  def ensure_not_referenced_by_any_line_item
+    return if line_items.empty?
+
+    errors.add(:base, "Line item(s) present")
+    throw :abort
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_02_084449) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_160535) do
+  create_table "carts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -20,4 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_02_084449) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
